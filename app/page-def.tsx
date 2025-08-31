17ee81ff9d279556109db7d015cf33169a7fa9d4
ralfpullerits <ralf.pullerits@gmail.com>
2025-09-01 00:14:40 +0300

    small fixes

--- app/page-def.tsx
+++ app/page-def.tsx
@@ -1,12 +1,14 @@
 import { DeployButton } from "@/components/deploy-button";
 import { EnvVarWarning } from "@/components/env-var-warning";
 import { AuthButton } from "@/components/auth-button";
+import { Hero } from "@/components/hero";
+import { ThemeSwitcher } from "@/components/theme-switcher";
+import { ConnectSupabaseSteps } from "@/components/tutorial/connect-supabase-steps";
+import { SignUpUserSteps } from "@/components/tutorial/sign-up-user-steps";
 import { hasEnvVars } from "@/lib/utils";
 import Link from "next/link";
 
-export default function ProtectedLayout({children,} // ← 1. **Destructuring**: extract `children` from the props object
-  : {children: React.ReactNode;}  // ← 2. **Type annotation**: props must be an object with `children` of type React.ReactNode
-) {
+export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <div className="flex-1 w-full flex flex-col gap-20 items-center">
@@ -22,7 +24,11 @@ export default function ProtectedLayout({children,} // ← 1. **Destructuring**:
           </div>
         </nav>
         <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          {children}
+          <Hero />
+          <main className="flex-1 flex flex-col gap-6 px-4">
+            <h2 className="font-medium text-xl mb-4">Next steps</h2>
+            {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+          </main>
         </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
@@ -37,6 +43,7 @@ export default function ProtectedLayout({children,} // ← 1. **Destructuring**:
               Supabase
             </a>
           </p>
+          <ThemeSwitcher />
         </footer>
       </div>
     </main>
